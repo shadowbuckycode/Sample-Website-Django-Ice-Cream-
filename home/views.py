c756de7943c369a956d1d6546107fa54169dd563
arpittripathi2007 <arpittripathi2007@gmail.com>
2021-09-13 12:50:41 +0530

    Django_Bharosa_Icecream_Project_Commit_1

---
+++ home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'name': 'SABKI MAA KA BHAROSAAAA!!!',
+        'extra': 'ar kya chahiye'
+    }
+    return render(request, 'index.html', context)
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
